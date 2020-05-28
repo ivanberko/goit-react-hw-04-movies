7ff9c825d88b21f68fed11a8a2c318788f5b9eab
Ivan Berk <berkoivan@gmail.com>
2020-05-28 23:44:22 +0300

    Updates

---
+++ precache-manifest.d5be8c59f564a7fb60133cefce55287e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6205259147366ed228f93c1e49b6e7ca",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "aebc63eb3bb4f207dd3d",
+    "url": "/goit-react-hw-04-movies/static/js/2.6f227506.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-04-movies/static/js/2.6f227506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265c6b8193fc1ab8835c",
+    "url": "/goit-react-hw-04-movies/static/js/main.25e40b64.chunk.js"
+  },
+  {
+    "revision": "a9fac4b4f93cc9ca4afe",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.2a7314cf.js"
+  }
+]);
